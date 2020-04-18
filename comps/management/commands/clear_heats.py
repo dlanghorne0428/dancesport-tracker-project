4370a5e6143e36065316ce5ff9b41bcd45a406f2
David Langhorne <david.b.langhorne@gmail.com>
2020-04-18 16:33:15 -0400

    Update clear_heats.py
    Delete all heat results

--- comps/management/commands/clear_heats.py
+++ comps/management/commands/clear_heats.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
-from comps.models import Heat
+from comps.models import Heat, HeatResult
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        HeatResult.objects.all().delete()
         Heat.objects.all().delete()
