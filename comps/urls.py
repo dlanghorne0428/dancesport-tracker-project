74e2533b2ccd803784dc9c86eaeeeea97c5d7fc1
David Langhorne <david.b.langhorne@gmail.com>
2020-04-18 16:30:56 -0400

    Add view_heats and heat_result URLs

--- comps/urls.py
+++ comps/urls.py
@@ -6,6 +6,8 @@ app_name = 'comps'
 urlpatterns = [
     path('', views.all_comps, name="all_comps"),
     path('<int:comp_id>/', views.detail, name="detail"),
-    path('heatlists/<int:comp_id>/', views.process_heatlists, name="process_heatlists"),    
+    path('view_heats/<int:comp_id>/', views.view_heats, name="view_heats"),
+    path('heat_results/<int:heat_id>/', views.heat_results, name="heat_results"),
     path('create/', views.createcomp, name='createcomp'),
+    path('process_heatlists/<int:comp_id>/', views.process_heatlists, name="process_heatlists"),
 ]
