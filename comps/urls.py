444a7fff2c9ca354445cc078fad263c0f18b0516
David Langhorne <david.b.langhorne@gmail.com>
2020-04-25 17:28:35 -0400

    Fixed url paths

--- comps/urls.py
+++ comps/urls.py
@@ -6,9 +6,11 @@ app_name = 'comps'
 urlpatterns = [
     path('', views.all_comps, name="all_comps"),
     path('<int:comp_id>/', views.detail, name="detail"),
-    path('heats/<int:comp_id>/', views.heats, name="heats"),
+    path('<int:comp_id>/heats', views.heats, name="heats"),
+    path('<int:comp_id>/load_dancers', views.load_dancers, name="load_dancers"),
+    path('<int:comp_id>/dancers', views.resolve_dancers, name="resolve_dancers"),
+    path('<int:comp_id>/load_heats', views.load_heats, name="load_heats"),
+    path('<int:comp_id>/mismatch_heats', views.resolve_mismatches, name="resolve_mismatches"),
     path('heat_entries/<int:heat_id>/', views.heat_entries, name="heat_entries"),
     path('create/', views.createcomp, name='createcomp'),
-    path('process_heatlists/<int:comp_id>/', views.process_heatlists, name="process_heatlists"),
-    path('resolve_mismatches/<int:comp_id>/', views.resolve_mismatches, name="resolve_mismatches"),
 ]
