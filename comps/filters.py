8f5f4f29cec8df8986e665b7d0aec8294f34e01c
David Langhorne <david.b.langhorne@gmail.com>
2020-04-28 17:46:41 -0400

    Added filter and form for Heat class

---
+++ comps/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+from .models import Heat
+
+class HeatFilter(django_filters.FilterSet):
+
+    info = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Heat
+        fields = ['category', 'style', 'base_value'  ]
