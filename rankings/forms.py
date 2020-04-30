a98c58da16dd2ae1a153284b3b8ce9aa967e9cfa
David Langhorne <david.b.langhorne@gmail.com>
2020-04-30 19:00:39 -0400

    Add couple from dancer page
    Also sort dancer choice fields by last name when adding couple

--- rankings/forms.py
+++ rankings/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.db import models
+from django.forms import ModelForm, ModelChoiceField
 from .models import Dancer, Couple
 
 class DancerForm(ModelForm):
@@ -7,6 +8,9 @@ class DancerForm(ModelForm):
         fields = ['name_first', 'name_middle', 'name_last', 'dancer_type']
 
 class CoupleForm(ModelForm):
+    dancer_1 = ModelChoiceField(queryset=Dancer.objects.order_by('name_last'))
+    dancer_2 = ModelChoiceField(queryset=Dancer.objects.order_by('name_last'))
+    # couple_type_field = models.CharField(max_length = 3, choices=Couple.COUPLE_TYPE_CHOICES)
     class Meta:
         model = Couple
         fields = ['dancer_1', 'dancer_2', 'couple_type']
