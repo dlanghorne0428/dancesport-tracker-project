cd906451bf68b3b6decd4b392e4531756955e776
David Langhorne <david.b.langhorne@gmail.com>
2020-05-03 15:25:04 -0400

    Initial commit

---
+++ comps/views/load_scoresheets.py
@@ -0,0 +1,14 @@
+from django.core import serializers
+from django.shortcuts import render, get_object_or_404
+from comps.models import Comp
+from comps.tasks import process_scoresheet_task
+
+def load_scoresheets(request, comp_id):
+    comp_objects = Comp.objects.filter(pk=comp_id)
+    if len(comp_objects) == 1:
+        comp=comp_objects[0]
+
+    comp_data = serializers.serialize("json", comp_objects)
+
+    result = process_scoresheet_task.delay(comp_data)
+    return render(request, 'comps/load_scoresheets.html', context={'task_id': result.task_id, 'comp': comp})
