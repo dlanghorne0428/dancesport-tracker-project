a33ba5bb14d0d73d66b8f7bce7febc1b72e7199b
David Langhorne <david.b.langhorne@gmail.com>
2020-05-23 19:00:00 -0400

    remove debug

--- comps/heatlist/heatlist.py
+++ comps/heatlist/heatlist.py
@@ -51,7 +51,6 @@ class Heatlist():
                 h = heats_in_database.first()
             else:
                 h = heat
-                print(h)
                 h.save()   # save the heat into the database
             return h
         else: # not a heat that we care about
