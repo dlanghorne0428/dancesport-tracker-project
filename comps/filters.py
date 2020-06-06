2146edb51dffb8c3850841f92ef3f8a3bae1d54c
David Langhorne <david.b.langhorne@gmail.com>
2020-06-05 21:59:48 -0400

    styling

--- comps/filters.py
+++ comps/filters.py
@@ -1,10 +1,13 @@
 import django_filters
+from django import forms
 from .models.heat import Heat
 
 class HeatFilter(django_filters.FilterSet):
 
     info = django_filters.CharFilter(lookup_expr='icontains')
+    heat_number = django_filters.NumberFilter(widget=forms.TextInput(attrs={'size': 5}))
+    base_value = django_filters.NumberFilter(widget=forms.TextInput(attrs={'size': 5}))
 
     class Meta:
         model = Heat
-        fields = ['category', 'style', 'heat_number', 'base_value'  ]
+        fields = ['category', 'style' ]
