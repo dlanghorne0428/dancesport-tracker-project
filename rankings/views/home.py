be731c648c13d0c52e987f7992fd969056af00cb
David Langhorne <david.b.langhorne@gmail.com>
2020-06-06 11:38:59 -0400

    Add home page; rankings on secondary page

--- rankings/views/home.py
+++ rankings/views/home.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render, redirect
-from rankings.models import Couple
-from comps.models.heat import Heat
+
 
 
 def home(request):
-    couple_types = Couple.COUPLE_TYPE_CHOICES
-    heat_couple_type = couple_types[0][0]
-    styles = Heat.DANCE_STYLE_CHOICES
-    heat_style = styles[0][0]
-
-    url_string = "rankings/?type=" + heat_couple_type + "&style=" + heat_style
-    return redirect(url_string)
+    return render(request, 'rankings/home.html')
