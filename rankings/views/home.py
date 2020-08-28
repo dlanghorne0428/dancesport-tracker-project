056535f73eb7242ed3f57ef0f352913937eb1945
David Langhorne <david.b.langhorne@gmail.com>
2020-08-27 23:23:30 -0400

    Add link to current comps on home page

--- rankings/views/home.py
+++ rankings/views/home.py
@@ -1,9 +1,10 @@
+import datetime
 from django.shortcuts import render, redirect
-
-
+from comps.models.comp import Comp
 
 def home(request):
-    return render(request, 'rankings/home.html')
+    current_comps = Comp.objects.filter(end_date__gte=datetime.date.today())
+    return render(request, 'rankings/home.html', {'current_comps': current_comps})
 
 
 def scoring(request):
