cc175695c22da0a923de7c799797d681997d446a
David Langhorne <david.b.langhorne@gmail.com>
2020-10-04 21:51:37 -0400

    FIx current comp list on home page
    Show comps for upcoming 5 days

--- rankings/views/home.py
+++ rankings/views/home.py
@@ -1,9 +1,10 @@
 import datetime
+from datetime import timedelta
 from django.shortcuts import render, redirect
 from comps.models.comp import Comp
 
 def home(request):
-    current_comps = Comp.objects.filter(end_date__gte=datetime.date.today())
+    current_comps = Comp.objects.filter(start_date__gte=datetime.date.today() - timedelta(days=2)).filter(end_date__lte=datetime.date.today() + timedelta(days=5))
     return render(request, 'rankings/home.html', {'current_comps': current_comps})
 
 
