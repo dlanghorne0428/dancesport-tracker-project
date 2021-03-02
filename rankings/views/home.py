d2cf4229404f4b6c3f8ecd56d561d08a345aa6a8
David Langhorne <david.b.langhorne@gmail.com>
2021-03-02 17:23:44 -0500

    don't display CANCELED comps on home page

--- rankings/views/home.py
+++ rankings/views/home.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, redirect
 from comps.models.comp import Comp
 
 def home(request):
-    current_comps = Comp.objects.filter(start_date__gte=datetime.date.today() - timedelta(days=2)).filter(end_date__lte=datetime.date.today() + timedelta(days=5))
+    active_comps = Comp.objects.exclude(location='CANCELED')
+    current_comps = active_comps.filter(start_date__gte=datetime.date.today() - timedelta(days=2)).filter(end_date__lte=datetime.date.today() + timedelta(days=5))
     return render(request, 'rankings/home.html', {'current_comps': current_comps})
 
 
