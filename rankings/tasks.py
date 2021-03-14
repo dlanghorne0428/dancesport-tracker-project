87bf45a4102978e1ab756f43adf6931109db3e0b
David Langhorne <david.b.langhorne@gmail.com>
2021-03-14 11:27:26 -0400

    Refactor - rating_stats module

--- rankings/tasks.py
+++ rankings/tasks.py
@@ -3,7 +3,7 @@ from celery_progress.backend import ProgressRecorder
 from django.core.cache import cache
 from comps.models import Heat_Entry
 from rankings.models import Dancer, Couple
-from rankings.couple_stats import couple_stats
+from rankings.rating_stats import couple_stats, instructor_stats
 from decimal import Decimal
 from operator import itemgetter
 import time
@@ -42,37 +42,22 @@ def calc_teacher_ratings(self, heat_style, cache_key):
 
     teachers = Dancer.objects.filter(dancer_type="PRO")
     progress_recorder.set_progress(0, len(teachers))
-    teacher_stats = list()
-    for t in teachers:
-        if Couple.objects.filter(couple_type="PAC").filter(dancer_2=t).count() > 0:
-            stats = {'teacher': t, 'event_count': 0, 'total_points': Decimal(0.00), 'rating': Decimal(0.00), 'index': 0}
-            teacher_stats.append(stats)
-
-    possible_matching_entries = Heat_Entry.objects.filter(couple__couple_type="PAC").filter(heat__style=heat_style)
-
+    teacher_stat_list = list()
     index = 0
-    for ts in teacher_stats:
-        entries = possible_matching_entries.filter(couple__dancer_2=ts['teacher'])
-        for e in entries:
-            if e.points is not None:
-                ts['event_count'] += 1
-                ts['total_points'] += e.points
-        if ts['event_count'] > 0:
-            ts['total_points'] = round(ts['total_points'], 2)
-            ts['rating'] = round(ts['total_points'] / ts['event_count'], 2)
-
+    for t in teachers:
+        teacher_stat_list.append(instructor_stats(t, heat_style))
         index += 1
         progress_recorder.set_progress(index, len(teachers))
 
-    teacher_stats.sort(key=itemgetter('rating'), reverse=True)
-    while teacher_stats[-1]['event_count'] == 0:
-        teacher_stats.pop()
-        if len(teacher_stats) == 0:
+    teacher_stat_list.sort(key=itemgetter('rating'), reverse=True)
+    while teacher_stat_list[-1]['event_count'] == 0:
+        teacher_stat_list.pop()
+        if len(teacher_stat_list) == 0:
             break
-    for i in range(len(teacher_stats)):
-        teacher_stats[i]['index'] = i + 1
+    for i in range(len(teacher_stat_list)):
+        teacher_stat_list[i]['index'] = i + 1
 
-    cache.set(cache_key, teacher_stats)
+    cache.set(cache_key, teacher_stat_list)
 
     result = index
     return result
