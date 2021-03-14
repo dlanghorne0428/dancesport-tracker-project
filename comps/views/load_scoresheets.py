b65aed64e26ce550a173710d28b093e97453fa58
David Langhorne <david.b.langhorne@gmail.com>
2021-03-14 11:26:41 -0400

    Clear results cache after loading scoresheet
    Adding scores will change results

--- comps/views/load_scoresheets.py
+++ comps/views/load_scoresheets.py
@@ -1,4 +1,5 @@
 from django.core import serializers
+from django.core.cache import cache
 from django.shortcuts import render, get_object_or_404
 from comps.models.comp import Comp
 from comps.tasks import process_scoresheet_task
@@ -14,4 +15,8 @@ def load_scoresheets(request, comp_id):
     comp_data = serializers.serialize("json", comp_objects)
 
     result = process_scoresheet_task.delay(comp_data)
+
+    # the rankings will have changed after loading these scoresheets, so clear out any cached rankings
+    cache.clear()
+
     return render(request, 'comps/load_scoresheets.html', context={'task_id': result.task_id, 'comp': comp})
