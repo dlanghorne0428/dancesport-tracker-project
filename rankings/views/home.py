9623a2b709e0a692697b51c1b35286d1da0e5efd
David Langhorne <david.b.langhorne@gmail.com>
2021-03-14 11:28:16 -0400

    Fix current comp calculations

--- rankings/views/home.py
+++ rankings/views/home.py
@@ -1,11 +1,13 @@
 import datetime
 from datetime import timedelta
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from comps.models.comp import Comp
 
 def home(request):
     active_comps = Comp.objects.exclude(location='CANCELED')
-    current_comps = active_comps.filter(start_date__gte=datetime.date.today() - timedelta(days=2)).filter(end_date__lte=datetime.date.today() + timedelta(days=5))
+    current_comps = active_comps.exclude(start_date__gt=datetime.date.today()).exclude(end_date__lt=datetime.date.today() - timedelta(days=2)).order_by('start_date') 
+
     return render(request, 'rankings/home.html', {'current_comps': current_comps})
 
 
