87655f066e34685aeb624b52412b32fb5fec25a1
David Langhorne <david.b.langhorne@gmail.com>
2021-06-09 23:36:53 -0400

    Repair scoresheet errors
    NO_ENTRIES_FOUND and NO_COUPLE_RESULT

--- comps/views/scoresheet_error_crud.py
+++ comps/views/scoresheet_error_crud.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
 from comps.models.comp import Comp
 from comps.models.heat import Heat
+from comps.models.heat_entry import Heat_Entry
 from comps.models.result_error import Result_Error
 
 
@@ -12,9 +13,25 @@ def delete_scoresheet_error(request, error_id):
     return redirect("comps:show_scoresheet_errors", comp.id)
 
 
+def repair_scoresheet_error(request, error_id):
+    scoresheet_error = get_object_or_404(Result_Error, pk=error_id)
+    if scoresheet_error.error == Result_Error.NO_COUPLE_RESULT:
+        heat_entry = Heat_Entry.objects.filter(heat=scoresheet_error.heat).filter(couple=scoresheet_error.couple)
+        if heat_entry is not None:
+            heat_entry.delete()
+    elif scoresheet_error.error == Result_Error.NO_ENTRIES_FOUND:
+        heat = scoresheet_error.heat
+        if heat is not None:
+            heat.delete()
+
+    comp = scoresheet_error.comp
+    scoresheet_error.delete()
+    return redirect("comps:show_scoresheet_errors", comp.id)
+
+
 def show_scoresheet_errors(request, comp_id):
     comp = get_object_or_404(Comp, pk=comp_id)
-    scoresheet_errors = Result_Error.objects.filter(comp=comp).order_by('heat')
+    scoresheet_errors = Result_Error.objects.filter(comp=comp).order_by('heat__time')
     paginator = Paginator(scoresheet_errors, 16)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
