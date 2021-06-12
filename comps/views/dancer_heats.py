4178b6ecf50a1228798bbbf8496d0cf6b9bc3bd7
David Langhorne <david.b.langhorne@gmail.com>
2021-06-11 21:28:27 -0400

    show couple on dancer_heats page

--- comps/views/dancer_heats.py
+++ comps/views/dancer_heats.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from comps.models.comp import Comp
-from comps.models.heat import Heat
+from comps.models.heat_entry import Heat_Entry
 from rankings.models import Dancer
 from comps.filters import HeatFilter
 
@@ -11,8 +11,8 @@ def dancer_heats(request, comp_id, dancer_id):
     comp = get_object_or_404(Comp, pk=comp_id)
     dancer = get_object_or_404(Dancer, pk=dancer_id)
 
-    heats = Heat.objects.filter(comp=comp).filter(Q(heat_entry__couple__dancer_1=dancer) | Q(heat_entry__couple__dancer_2=dancer)).distinct().order_by('time')
-    paginator = Paginator(heats, 16)
+    heat_entries = Heat_Entry.objects.filter(heat__comp=comp).filter(Q(couple__dancer_1=dancer) | Q(couple__dancer_2=dancer)).distinct().order_by('heat__time')
+    paginator = Paginator(heat_entries, 16)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, "comps/dancer_heats.html", {'comp': comp, 'page_obj': page_obj, 'dancer': dancer})
