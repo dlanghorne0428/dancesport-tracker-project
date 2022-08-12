82a3828b3400f0aa06d9c5411af12c12ce3c8815
David Langhorne <david.b.langhorne@gmail.com>
2022-08-12 18:27:15 -0400

    Multiple improvements
    Change initial elo values for new couples
    Only apply elo changes once per heat
    Track number of events per couple

--- comps/views/update_elo_ratings.py
+++ comps/views/update_elo_ratings.py
@@ -9,19 +9,25 @@ from comps.models.heat_entry import Heat_Entry
 from rankings.models.elo_rating import EloRating
 
 def initial_elo_rating(category, info):
-    if category == "PH":
-        return 1500
     info_up = info.upper()
+    if category == "PH":
+        if "RISING STAR" in info_up or "RS" in info_up or "NOVICE" in info_up or "BASIC" in info_up or "PRE-CHAMP" in info_up:
+            return 1500
+        else:
+            return 1750
+
     if 'GOLD' in info_up:
-        return 1500
+        return 1250
     if 'SILVER' in info_up:
         return 1000
     if 'BRONZE' in info_up or 'NEWCOMER' in info_up or 'NOVICE' in info_up:
-        return 500
+        return 750
     if 'PRE-CHAMP' in info_up or 'PRE CHAMP' in info_up or 'PRECHAMP' in info_up or 'RISING STAR' in info_up or 'CLOSED' in info_up:
         return 1000 
     if 'OPEN' in info_up or 'ADVANCED' in info_up or 'SCHOLAR' in info_up or 'CHAMP' in info_up or 'SLAM' in info_up or 'DSS' in info_up or "DANCESPORT SERIES" in info_up:
-        return 1500
+        return 1250
+    if 'CHALLENGE' in info_up:
+        return 1000
     else:
         print('Unknown level for ' + info)
         return None
@@ -38,30 +44,28 @@ def update_elo_ratings(request, comp_id):
     # initialize the multi-elo entries
     elo = MultiElo(score_function_base=1.25)
     
-    heats = Heat.objects.filter(comp=comp).order_by('time')
-    # first, ensure we can determine a valid initial elo rating for this heat
-    for h in heats:
-        # get the entries for this heat in order of their results
-        entries = Heat_Entry.objects.filter(heat=h)
-        
-        # if there are multiple entries, update elo ratings for each entry
-        if len(entries) > 1:
-            # determine initial elo rating for entries with no previous rating
-            if initial_elo_rating(h.category, h.info) is None:
-                heat_data = list()
-                heat_data.append({'heat': h, 'entries': entries, 'results_available': False, 'error': "Unable to determine initial elo rating"})
-                return render(request, 'comps/heat.html', {'comp': comp, 'heat_data': heat_data, 'show_edit_button': True})
+    heats = Heat.objects.filter(comp=comp).order_by('time', 'heat_number')
     
     # for each heat in this comp
     for h in heats:
+        
+        # skip this heat if elo ratings have already been applied
+        if h.elo_applied:
+            continue
+        
         # get the entries for this heat in order of their results
         entries = Heat_Entry.objects.filter(heat=h).order_by('-points')
         
         # if there are multiple entries, update elo ratings for each entry
         if len(entries) > 1:
             # determine initial elo rating for entries with no previous rating
-            initial_rating = initial_elo_rating(h.category, h.info)      
-            #print(str(h) + ' ' + str(h.time) + ' ' + h.info + ' ' + h.style +  ' ' + str(initial_rating))
+            if h.initial_elo_value is None:
+                heat_data = list()
+                heat_data.append({'heat': h, 'entries': entries, 'results_available': False, 'error': "Unable to determine initial elo rating"})
+                return render(request, 'comps/heat.html', {'comp': comp, 'heat_data': heat_data, 'show_edit_button': True})
+            else:
+                initial_rating = h.initial_elo_value   
+                #print(str(h) + ' ' + str(h.time) + ' ' + h.info + ' ' + h.style +  ' ' + str(initial_rating))
             
             rating_list = list()
             elo_inputs = list()
@@ -86,9 +90,17 @@ def update_elo_ratings(request, comp_id):
             new_ratings = elo.get_new_ratings(numpy.array(elo_inputs))
             
             for index in range(len(rating_list)):
-                #print('  ' + str(rating_list[index]) + ' ' + str(round(new_ratings[index], 2)))
+                difference = new_ratings[index] - elo_inputs[index]
+                #print('  ' + str(rating_list[index]) + ' ' + str(round(new_ratings[index], 2)) + ' ' + str(round(difference, 2)))
+
+                entries[index].elo_adjust = difference
+                entries[index].save()
                 rating_list[index].value = new_ratings[index]
+                rating_list[index].num_events += 1
                 rating_list[index].save()
+                
+            h.elo_applied = True
+            h.save()
                 
     comp.process_state = Comp.ELO_RATINGS_UPDATED
     comp.save()
