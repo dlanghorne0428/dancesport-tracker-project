df285c9441238dcb7af2643e95b2e7ca764550c6
David Langhorne <david.b.langhorne@gmail.com>
2022-08-12 18:04:16 -0400

    Show initial elo value for heats

--- comps/views/comp_heats.py
+++ comps/views/comp_heats.py
@@ -7,8 +7,12 @@ from comps.filters import HeatFilter
 
 def comp_heats(request, comp_id):
     comp = get_object_or_404(Comp, pk=comp_id)
-    f = HeatFilter(request.GET, queryset=Heat.objects.filter(comp=comp).order_by('time', 'extra', 'info'))
+    
+    # only show elo column for valid users
+    show_elo = request.user.is_superuser
+    
+    f = HeatFilter(request.GET, queryset=Heat.objects.filter(comp=comp).order_by('time', 'heat_number', 'info'))
     paginator = Paginator(f.qs, 16)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, "comps/comp_heats.html", {'comp': comp, 'page_obj': page_obj, 'filter': f})
+    return render(request, "comps/comp_heats.html", {'comp': comp, 'page_obj': page_obj, 'show_elo': show_elo, 'filter': f})
