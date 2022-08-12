8ddccfe9531e07c0df80d24a9227887903bc03a1
David Langhorne <david.b.langhorne@gmail.com>
2022-08-12 18:18:19 -0400

    Filter on heat entries
    Allows results and elo rating changes to be shown

--- comps/views/couple_heats.py
+++ comps/views/couple_heats.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from comps.models.comp import Comp
 from comps.models.heat import Heat
+from comps.models.heat_entry import Heat_Entry
 from rankings.models.couple import Couple
 from comps.filters import HeatFilter
 
@@ -13,8 +14,8 @@ def couple_heats(request, comp_id, couple_id):
     comp = get_object_or_404(Comp, pk=comp_id)
     couple = get_object_or_404(Couple, pk=couple_id)
 
-    heats = Heat.objects.filter(comp=comp).filter(heat_entry__couple=couple).distinct().order_by('time')
-    paginator = Paginator(heats, 16)
+    heat_entries = Heat_Entry.objects.filter(heat__comp=comp, couple=couple).order_by('heat__time')
+    paginator = Paginator(heat_entries, 16)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, "comps/couple_heats.html", {'comp': comp, 'page_obj': page_obj, 'couple': couple, 'show_add_button': show_add_button})
