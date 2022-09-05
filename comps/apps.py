79e7359e5f8e3ec4554463aaee5d057ffd58a7f5
David Langhorne <david.b.langhorne@gmail.com>
2022-09-05 13:41:09 -0400

    fix migrations for local

--- comps/apps.py
+++ comps/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CompsConfig(AppConfig):
     name = 'comps'
-    #default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
