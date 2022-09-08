d29ff619a635bb72458263f26575b04c9482305b
David Langhorne <david.b.langhorne@gmail.com>
2022-09-08 19:00:30 -0400

    put back django-herokui

--- dancesport_tracker/settings.py
+++ dancesport_tracker/settings.py
@@ -159,6 +159,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-#import django_heroku
-#django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
