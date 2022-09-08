202eb104d416b30d17c65a856ee544437792cf6d
David Langhorne <david.b.langhorne@gmail.com>
2022-09-08 18:14:31 -0400

    remove debug print statements

--- rankings/views/home.py
+++ rankings/views/home.py
@@ -6,10 +6,9 @@ from comps.models.comp import Comp
 
 def home(request):
     current_comps = list()
-    #active_comps = Comp.objects.exclude(location='CANCELED')
-    #current_comps = active_comps.exclude(start_date__gt=datetime.date.today()).exclude(end_date__lt=datetime.date.today() - timedelta(days=2)).order_by('start_date') 
+    active_comps = Comp.objects.exclude(location='CANCELED')
+    current_comps = active_comps.exclude(start_date__gt=datetime.date.today()).exclude(end_date__lt=datetime.date.today() - timedelta(days=2)).order_by('start_date') 
 
-    print("Test - can you see this?", flush=True)
     return render(request, 'rankings/home.html', {'current_comps': current_comps})
 
 
