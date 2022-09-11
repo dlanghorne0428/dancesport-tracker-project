092dc71bed8d6d823e0008dedf6e55d572097161
David Langhorne <david.b.langhorne@gmail.com>
2022-09-10 20:55:42 -0400

    filter widget styling

--- comps/filters.py
+++ comps/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from django import forms
 from .models.comp import Comp
-from .models.heat import Heat
+from .models.heat import Heat, DANCE_STYLE_CHOICES, CATEGORY_CHOICES
 
 class CompFilter(django_filters.FilterSet):
 
@@ -11,10 +11,8 @@ class CompFilter(django_filters.FilterSet):
 
 class HeatFilter(django_filters.FilterSet):
 
-    info = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':12}))
+    info = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':16, 'placeholder':'keyword'}))
     heat_number = django_filters.NumberFilter(widget=forms.TextInput(attrs={'size': 5}))
-    base_value = django_filters.NumberFilter(widget=forms.TextInput(attrs={'size': 5}))
+    style = django_filters.ChoiceFilter(choices=DANCE_STYLE_CHOICES, empty_label="All Styles") 
+    category = django_filters.ChoiceFilter(choices=CATEGORY_CHOICES, empty_label="ALL")
 
-    class Meta:
-        model = Heat
-        fields = ['category', 'style' ]
