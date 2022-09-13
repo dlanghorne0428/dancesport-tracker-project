48ff298a433ca767ca283961673886280edef252
David Langhorne <david.b.langhorne@gmail.com>
2022-09-12 20:04:12 -0400

    Iss 187 format date (#188)
    * don't create duplicate comp_couples
    
    * refactor category_choices
    
    * Heats vs. Results for standard user
    
    * Mag Glass styling
    
    * date/time format
    
    * change logic for event header display

--- comps/filters.py
+++ comps/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from django import forms
 from .models.comp import Comp
-from .models.heat import Heat, DANCE_STYLE_CHOICES, CATEGORY_CHOICES
+from .models.heat import Heat, DANCE_STYLE_CHOICES
 
 class CompFilter(django_filters.FilterSet):
 
@@ -14,5 +14,5 @@ class HeatFilter(django_filters.FilterSet):
     info = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':16, 'placeholder':'keyword'}))
     heat_number = django_filters.NumberFilter(widget=forms.TextInput(attrs={'size': 5}))
     style = django_filters.ChoiceFilter(choices=DANCE_STYLE_CHOICES, empty_label="All Styles") 
-    category = django_filters.ChoiceFilter(choices=CATEGORY_CHOICES, empty_label="ALL")
+    category = django_filters.ChoiceFilter(choices=Heat.CATEGORY_CHOICES, empty_label="ALL")
 
