e6d17ff516bd1a468ba8bdda197088874fd45c47
David Langhorne <david.b.langhorne@gmail.com>
2022-09-13 20:15:54 -0400

    refactor DANCER_TYPE_CHOICES

--- rankings/filters.py
+++ rankings/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from django import forms
-from .models import Dancer, DANCER_TYPE_CHOICES
+from .models import Dancer
 
 class DancerFilter(django_filters.FilterSet):
     name_last = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':16, 'placeholder': 'last name'}))
-    dancer_type = django_filters.ChoiceFilter(choices=DANCER_TYPE_CHOICES, empty_label="ALL")
+    dancer_type = django_filters.ChoiceFilter(choices=Dancer.DANCER_TYPE_CHOICES, empty_label="ALL")
