98a59e1e301ba5bdac948c9eb736f3a5b1d350d8
David Langhorne <david.b.langhorne@gmail.com>
2022-09-19 20:16:00 -0400

    use background task to recalc

--- rankings/views/recalc_elo_ratings.py
+++ rankings/views/recalc_elo_ratings.py
@@ -2,16 +2,18 @@ from django.shortcuts import redirect, render, get_object_or_404
 
 from comps.models.comp import Comp
 from comps.tasks import update_elo_ratings_for_comps
+from rankings.models.elo_rating import EloRating
 
 def recalc_elo_ratings(request):
     if not request.user.is_superuser:
         return render(request, 'rankings/permission_denied.html') 
     
+    comp_list = list()
     comps = Comp.objects.all().order_by('start_date')
     for c in comps:
-        if c.process_state != Comp.RESULTS_RESOLVED:
-            print("Incorrect state " + str(c))
-        #else
-            update_elo_ratings_for_comps(c)
+        if c.process_state in [Comp.RESULTS_RESOLVED, Comp.ELO_RATINGS_UPDATED, Comp.COMPLETE]:
+            comp_list.append(c.id)
             
-    return redirect('comps:all_comps')
+    result = update_elo_ratings_for_comps.delay(comp_list)
+    
+    return render(request, 'comps/update_elo_ratings.html', context={'task_id': result.task_id })
