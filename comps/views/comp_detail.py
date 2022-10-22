67a1dd64601cb445350c04ef2d19a3e22277cbaa
David Langhorne <david.b.langhorne@gmail.com>
2022-10-21 20:27:57 -0400

    check if user is authenicated

--- comps/views/comp_detail.py
+++ comps/views/comp_detail.py
@@ -3,11 +3,14 @@ from comps.models.comp import Comp
 
 def comp_detail(request, comp_id):
     # only show load dancer, load heat buttons for valid users
-    show_load_buttons = request.user.is_superuser
-    show_load_time = False
+    show_load_buttons = False
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            show_load_buttons = True
 
     comp = get_object_or_404(Comp, pk=comp_id)
     
+    show_load_time = False
     if comp.heatsheet_load_time is not None:
         if comp.heatsheet_load_time > Comp.default_time:
             show_load_time = True
