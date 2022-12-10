fcca57a777d2cbf425d7591a4392622caf61bb6b
David Langhorne <david.b.langhorne@gmail.com>
2022-12-10 15:26:43 -0500

    remove heat_list_dancers with no comp

--- rankings/views/alias_dancers.py
+++ rankings/views/alias_dancers.py
@@ -11,6 +11,12 @@ from rankings.models import Dancer
 def alias_dancers(request, level=2):
     if not request.user.is_superuser:
         return render(request, 'rankings/permission_denied.html')
+    
+    no_comp_list = Heatlist_Dancer.objects.filter(comp=None)
+    if len(no_comp_list) > 0:
+        print("Removing " + len(no_comp_list) + 'entries')
+        for d in no_comp_list:
+            d.delete()
 
     alias_dancers = Heatlist_Dancer.objects.exclude(alias__isnull=True).order_by('name', 'comp__start_date')
     if level == 2:
