9edbd6815092bd5d45cdd60400bf0d287eabd6bd
David Langhorne <david.b.langhorne@gmail.com>
2025-06-14 11:55:30 -0400

    add debug

--- rankings/couple_matching.py
+++ rankings/couple_matching.py
@@ -55,6 +55,7 @@ def find_couple_close_match(dancer, partner):
     partial_matching_couples = list()
 
     dancers = Dancer.objects.filter(name_last = dancer_last, name_first__istartswith=dancer.first[0])
+    print(dancers)
     partial_matches = find_last_name_matches(dancers, dancer.code, partner.code)
     for p in partial_matches:
         partial_matching_couples = append_to_match_list(partial_matching_couples, p)
